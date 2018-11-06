286e91d1efd80ab890fd572b971dca14e57bf475
gene-radcliffe <g_radcliffe@hotmail.com>
2018-11-05 21:11:26 -0500

    Updates

---
+++ precache-manifest.cbaacd963b45606b36d6c8709d2a546f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d86f2391166c1df94987",
+    "url": "/static/js/main.d86f2391.chunk.js"
+  },
+  {
+    "revision": "9fc667a21de85938a256",
+    "url": "/static/js/1.9fc667a2.chunk.js"
+  },
+  {
+    "revision": "d86f2391166c1df94987",
+    "url": "/static/css/main.7f0cc958.chunk.css"
+  },
+  {
+    "revision": "9fc667a21de85938a256",
+    "url": "/static/css/1.a1a4f3bd.chunk.css"
+  },
+  {
+    "revision": "6eb122d9be746d75ac81f3a78a1b0b6c",
+    "url": "/index.html"
+  }
+];
